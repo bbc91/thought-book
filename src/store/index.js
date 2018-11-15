535a524371f73ae1833b9fd7306e4e83af609820
bbc91 <bbcto91@gmail.com>
2018-11-15 22:06:48 +0200

    Add vuejs with firebase

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+import auth from './modules/auth'
+
+export default new Vuex.Store({
+    state: {},
+    modules: {
+        auth
+    },
+    mutations: {},
+    actions: {}
+})
---
+++ src/store/modules/auth.js
@@ -0,0 +1,17 @@
+const state = () => ({
+    user: null
+});
+
+const getters = {};
+
+const actions = {};
+
+const mutations = {};
+
+export default {
+    namespaced: false,
+    state,
+    getters,
+    actions,
+    mutations
+}
