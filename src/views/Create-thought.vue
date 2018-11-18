cabe08fbf80a5cd8bf41cd2fa4a0dbd4a6fcf74e
bbc91 <bbcto91@gmail.com>
2018-11-18 13:17:57 +0200

    Add basic wizard for steps.

--- src/views/Create-thought.vue
+++ src/views/Create-thought.vue
@@ -1,19 +1,67 @@
 <template>
     <div class="register">
-        Create thought
-        <button class="btn btn-info" @click="createThought()">Изпрати</button>
+        <h4>Добавяне на мисъл</h4>
+            <vue-good-wizard
+                    finalStepLabel="Изпрати"
+                    nextStepLabel="Напред"
+                    previousStepLabel="Назад"
+                    :steps="steps"
+                    :onNext="nextClicked"
+                    :onBack="backClicked">
+                <div slot="page1">
+                    <h4>Step 1</h4>
+                    <p>This is step 1</p>
+                </div>
+                <div slot="page2">
+                    <h4>Step 2</h4>
+                    <p>This is step 2</p>
+                </div>
+                <div slot="page3">
+                    <h4>Step 3</h4>
+                    <p>This is step 3</p>
+                </div>
+                <div slot="page4">
+                    <h4>Step 4</h4>
+                    <p>This is step 4</p>
+                </div>
+            </vue-good-wizard>
     </div>
 </template>
 
 <script>
     // @ is an alias to /src
     // import HelloWorld from '@/components/HelloWorld.vue'
+    import {GoodWizard} from 'vue-good-wizard';
 
     export default {
         name: 'create-thought',
-        components: {},
+        components: {
+            'vue-good-wizard': GoodWizard,
+        },
         data() {
-            return {}
+            return {
+                steps: [
+                    {
+                        label: '',
+                        slot: 'page1',
+                    },
+                    {
+                        label: '',
+                        slot: 'page2',
+                    },
+                    {
+                        label: '',
+                        slot: 'page3',
+                    },
+                    {
+                        label: '',
+                        slot: 'page4',
+                        options: {
+                            // nextDisabled: true, // control whether next is disabled or not
+                        },
+                    }
+                ],
+            }
         },
         computed: {
             user() {
@@ -30,6 +78,19 @@
                     console.log('thought added.');
                     self.$router.push({name: 'diary', params: {success: true}})
                 })
+            },
+            nextClicked(currentPage) {
+                console.log('next clicked', currentPage);
+
+                if (currentPage === this.steps.length - 1) {
+                    this.createThought();
+                }
+
+                return true; //return false if you want to prevent moving to next page
+            },
+            backClicked(currentPage) {
+                console.log('back clicked', currentPage);
+                return true; //return false if you want to prevent moving to previous page
             }
         },
         created() {
