8eabf4da7d2928f95d88259e8d8a1bdf55ef6f4b
bbc91 <bbcto91@gmail.com>
2018-11-18 16:20:00 +0200

    add automatic thoughts and rating.

--- src/views/Create-thought.vue
+++ src/views/Create-thought.vue
@@ -27,12 +27,12 @@
                 <br>
                 <p style="text-align: center">Дайте проценти на чувствата които изпивате в ситуацията.</p>
                 <div class="row">
-                    <div class="col-md-3 emotion">
+                    <div class="col-md-3 emotion" v-for="(emotion, index) in thought.emotions" :key="index">
                         <circle-slider
                                 progressColor="#17a2b8 "
                                 knobColor="#17a2b8"
                                 circleColor="#ccc"
-                                v-model="thought.emotions.fear"
+                                v-model="emotion.percent"
                                 :side="150"
                                 :min="0"
                                 :max="100"
@@ -41,131 +41,53 @@
                                 :progress-width-rel="10"
                                 :knob-radius="10"
                         ></circle-slider>
-                        Страх {{thought.emotions.fear}}%
-                    </div>
-                    <div class="col-md-3 emotion">
-                        <circle-slider
-                                progressColor="#17a2b8 "
-                                knobColor="#17a2b8"
-                                circleColor="#ccc"
-                                v-model="thought.emotions.worry"
-                                :side="150"
-                                :min="0"
-                                :max="100"
-                                :step-size="5"
-                                :circle-width-rel="20"
-                                :progress-width-rel="10"
-                                :knob-radius="10"
-                        ></circle-slider>
-                        Притеснение {{thought.emotions.worry}}%
-                    </div>
-                    <div class="col-md-3 emotion">
-                        <circle-slider
-                                progressColor="#17a2b8 "
-                                knobColor="#17a2b8"
-                                circleColor="#ccc"
-                                v-model="thought.emotions.humiliation"
-                                :side="150"
-                                :min="0"
-                                :max="100"
-                                :step-size="5"
-                                :circle-width-rel="20"
-                                :progress-width-rel="10"
-                                :knob-radius="10"
-                        ></circle-slider>
-                        Унижение {{thought.emotions.humiliation}}%
-                    </div>
-                    <div class="col-md-3 emotion">
-                        <circle-slider
-                                progressColor="#17a2b8 "
-                                knobColor="#17a2b8"
-                                circleColor="#ccc"
-                                v-model="thought.emotions.panic"
-                                :side="150"
-                                :min="0"
-                                :max="100"
-                                :step-size="5"
-                                :circle-width-rel="20"
-                                :progress-width-rel="10"
-                                :knob-radius="10"
-                        ></circle-slider>
-                        Паника {{thought.emotions.panic}}%
-                    </div>
-                </div>
-                <div class="row" style="margin-top: 15px; margin-bottom: 15px;">
-                    <div class="col-md-3 emotion">
-                        <circle-slider
-                                progressColor="#17a2b8 "
-                                knobColor="#17a2b8"
-                                circleColor="#ccc"
-                                v-model="thought.emotions.pain"
-                                :side="150"
-                                :min="0"
-                                :max="100"
-                                :step-size="5"
-                                :circle-width-rel="20"
-                                :progress-width-rel="10"
-                                :knob-radius="10"
-                        ></circle-slider>
-                        Болка {{thought.emotions.pain}}%
-                    </div>
-                    <div class="col-md-3 emotion">
-                        <circle-slider
-                                progressColor="#17a2b8 "
-                                knobColor="#17a2b8"
-                                circleColor="#ccc"
-                                v-model="thought.emotions.anger"
-                                :side="150"
-                                :min="0"
-                                :max="100"
-                                :step-size="5"
-                                :circle-width-rel="20"
-                                :progress-width-rel="10"
-                                :knob-radius="10"
-                        ></circle-slider>
-                        Гняв {{thought.emotions.anger}}%
-                    </div>
-                    <div class="col-md-3 emotion">
-                        <circle-slider
-                                progressColor="#17a2b8 "
-                                knobColor="#17a2b8"
-                                circleColor="#ccc"
-                                v-model="thought.emotions.apathy"
-                                :side="150"
-                                :min="0"
-                                :max="100"
-                                :step-size="5"
-                                :circle-width-rel="20"
-                                :progress-width-rel="10"
-                                :knob-radius="10"
-                        ></circle-slider>
-                        Апатия {{thought.emotions.apathy}}%
-                    </div>
-                    <div class="col-md-3 emotion">
-                        <circle-slider
-                                progressColor="#17a2b8 "
-                                knobColor="#17a2b8"
-                                circleColor="#ccc"
-                                v-model="thought.emotions.sadness"
-                                :side="150"
-                                :min="0"
-                                :max="100"
-                                :step-size="5"
-                                :circle-width-rel="20"
-                                :progress-width-rel="10"
-                                :knob-radius="10"
-                        ></circle-slider>
-                        Тъга {{thought.emotions.sadness}}%
+                        {{emotion.name}} {{emotion.percent}}%
                     </div>
                 </div>
             </div>
             <div slot="page3">
-                <h4>Step 3</h4>
-                <p>This is step 3</p>
+                <h4 style="text-align: center;">Добавяне на автоматични мисли</h4>
+                <br>
+                <p style="text-align: center">Добавете всички автоматични мисли които изникнаха в главата ви в дадената
+                    ситуация.</p>
+                <div class="my-3">
+                    <b-btn v-b-popover.hover="'“Дори съпругата ми вече не се нуждае от мен.” – тъга 50% \n “Всички си прекараха по-добре, защото мен не нямаше сред тях.” – тъга 90%'"
+                           title="Автоматични мисли:">
+                        Виж пример
+                    </b-btn>
+                </div>
+                <div class="form-group">
+                    <label for="addAutomaticThought">Автоматична мисъл</label>
+                    <textarea placeholder="Добави автоматична мисъл" id="addAutomaticThought" class="form-control"
+                              rows="3" v-model="automaticThoughText"></textarea>
+                </div>
+                <div class="form-group">
+                    <label>Посочете коя от емоциите които изпитвате е породена от тази автоматична мисъл</label>
+                    <br>
+                    <div v-if="emotion.percent" v-for="(emotion, index) in thought.emotions">
+                        <label><input v-model="automaticThoughtEmotion" :value="emotion" type="radio"
+                                      name="automaticThoughtEmotion">
+                            {{emotion.name}} - {{emotion.percent}}%</label>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <button @click="addAutomaticThought()" class="btn btn-info">Добави</button>
+                </div>
+                <div v-for="(automaticThought, index) in thought.automaticThoughts" :key="index">
+                    <p>{{automaticThought.content}} | {{automaticThought.emotion.name}} -
+                        {{automaticThought.emotion.percent}}%</p>
+                </div>
             </div>
             <div slot="page4">
-                <h4>Step 4</h4>
-                <p>This is step 4</p>
+                <h4 style="text-align: center;">Идентифициране на автоматични мисли</h4>
+                <br>
+                <!--<p style="text-align: center">Добавете всички автоматични мисли които изникнаха в главата ви в дадената-->
+                <!--ситуация.</p>-->
+                <div v-for="(automaticThought, index) in thought.automaticThoughts" :key="index">
+                    <p :class="{'hot': automaticThought.hot}">{{automaticThought.content}} |
+                        {{automaticThought.emotion.name}} -
+                        {{automaticThought.emotion.percent}}%</p>
+                </div>
             </div>
         </vue-good-wizard>
     </div>
@@ -185,8 +107,44 @@
             return {
                 thought: {
                     situation: '',
-                    emotions: {}
+                    emotions: [
+                        {
+                            name: 'Страх',
+                            percent: 0
+                        },
+                        {
+                            name: 'Притеснение',
+                            percent: 0
+                        },
+                        {
+                            name: 'Унижение',
+                            percent: 0
+                        },
+                        {
+                            name: 'Паника',
+                            percent: 0
+                        },
+                        {
+                            name: 'Болка',
+                            percent: 0
+                        },
+                        {
+                            name: 'Гняв',
+                            percent: 0
+                        },
+                        {
+                            name: 'Апатия',
+                            percent: 0
+                        },
+                        {
+                            name: 'Тъга',
+                            percent: 0
+                        }
+                    ],
+                    automaticThoughts: []
                 },
+                automaticThoughText: '',
+                automaticThoughtEmotion: null,
                 steps: [
                     {
                         label: '',
@@ -216,6 +174,16 @@
             }
         },
         methods: {
+            addAutomaticThought() {
+                let automaticThought = {
+                    content: this.automaticThoughText,
+                    emotion: this.automaticThoughtEmotion,
+                    hot: false
+                };
+                this.automaticThoughText = '';
+                this.automaticThoughtEmotion = null;
+                this.thought.automaticThoughts.push(automaticThought);
+            },
             createThought() {
                 let self = this;
                 this.$store.dispatch('createThought', {
@@ -233,7 +201,26 @@
                     this.createThought();
                 }
 
+                if (currentPage === 2) {
+                    this.calculateHotThought();
+                }
+
                 return true; //return false if you want to prevent moving to next page
+            },
+            calculateHotThought() {
+                console.log('calculation hot thought');
+                let max = null;
+                for (let i = 0; i < this.thought.automaticThoughts.length; i++) {
+                    if (!max) {
+                        max = this.thought.automaticThoughts[0];
+                    } else {
+                        if (this.thought.automaticThoughts[i].emotion.percent >= max.emotion.percent) {
+                            max = this.thought.automaticThoughts[i];
+                        }
+                    }
+                }
+                max.hot = true;
+                this.$forceUpdate();
             },
             backClicked(currentPage) {
                 console.log('back clicked', currentPage);
